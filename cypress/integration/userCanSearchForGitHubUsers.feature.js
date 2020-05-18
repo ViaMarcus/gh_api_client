969e384f4bb5cd24bc8b852b7d4183f815f4c550
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-18 11:01:30 +0200

    adding more tests, for empty and big results, and fixtures for those tests

--- cypress/integration/userCanSearchForGitHubUsers.feature.js
+++ cypress/integration/userCanSearchForGitHubUsers.feature.js
@@ -1,8 +1,73 @@
-describe('User can search for GitHub users', () => {
+describe("User can search for GitHub users", () => {
   beforeEach(() => {
-    cy.server()
-    cy.visit('/')
-    cy.route()
-    cy.route()
-  })
-})
+    cy.server();
+    cy.visit("/");
+    cy.route({
+      method: "GET",
+      url: "https://api.github.com/users/mojombo",
+      response: "fixture: mojombo_info.json",
+    });
+    cy.route({
+      method: "GET",
+      url: "https://api.github.com/search/users?q=tom+repos",
+      response: "fixture: search_result.json",
+    });
+  });
+
+  describe("user can search", () => {
+    beforeEach(() => {
+      cy.get("input#search-field").type("tom repos");
+      cy.get("button#search-submit").click();
+    });
+
+    it("displays the amoubt of results", () => {
+      cy.get("p#search-message").should("contain", "8 results");
+    });
+
+    it("displays the first result", () => {
+      cy.get("result-1").should("contain", "mojombo");
+    });
+
+    it("displays the second result", () => {
+      cy.get("result-2").should("contain", "tmcw");
+    });
+  });
+
+  describe("user can get empty results", () => {
+    it('says there are no results', () => {
+      cy.route({
+        method: "GET",
+        url: "https://api.github.com/search/users?q=whowouldevensearchforthis",
+        response: "fixture: empty_search.json",
+      });
+      cy.get("input#search-field").type("whowouldevensearchforthis");
+      cy.get("button#search-submit").click();
+      cy.get('#search-message').should("contain", "No results")
+    })
+  });
+
+  describe("user can get too many results", () => {
+    it('says there are hidden results', () => {
+      cy.route({
+        method: "GET",
+        url: "https://api.github.com/search/users?q=tom",
+        response: "fixture: big_search.json",
+      });
+      cy.get("input#search-field").type("tom");
+      cy.get("button#search-submit").click();
+      cy.get('#search-message').should("contain", "Showing result 1-30 of a total 112272. You should perhaps narrow your search")
+    })
+  });
+
+  describe("user can visit the results", () => {
+    beforeEach(() => {
+      cy.get("input#search-field").type("tom repos");
+      cy.get("button#search-submit").click();
+    });
+
+    it("can visit the first person", () => {
+      cy.get("#mojambo").click();
+      cy.get("#profile").should("contain", "Name: Tom Preston-Werner");
+    });
+  });
+});
